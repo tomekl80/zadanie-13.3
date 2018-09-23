7fe383a91b98d52fb9d0a42584c0336bce963d51
Tomasz Lis <tomasz.lis@o2.pl>
2018-09-23 23:53:34 +0200

    Final code of excercise 13.3
    *Change syntax if/else to switch statement

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use scrict';
+
+// Enkodowanie przyjmowanych danych
+process.stdin.setEncoding('utf-8');
+
+// Ustawienie nasłuchiwania na zdarzenia odczytu
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if(input !== null) {
+    	var instruction = input.toString().trim();
+    	switch (instruction) {
+    		case '/exit':
+    			process.stdout.write('Quiting app!\n');
+    			process.exit();
+    			break;
+			case '/processEnv':
+				process.stdout.write('\nNode.js Version\n')
+				process.stdout.write(process.version);
+				process.stdout.write('\n\nOS Version\n');
+				process.stdout.write(process.env.OS);
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+    	}
+    }
+});
